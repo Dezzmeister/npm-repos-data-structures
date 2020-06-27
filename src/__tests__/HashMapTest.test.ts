ce511ef693bc6afe44cd920d22c4c34555552208
Joe Desmond <joseph.desmond@grabcad.com>
2020-06-27 16:51:43 -0400

    Added unit tests

---
+++ src/__tests__/HashMapTest.test.ts
@@ -0,0 +1,62 @@
+import { HashMap } from "../";
+import Num from "../test-objects/TestObjects";
+
+test("Collision Test", () => {
+	const map = new HashMap<Num, string>(16, 1.0);
+	const five = new Num(5);
+	const twentyOne = new Num(21);
+
+	map.put(five, "five");
+	map.put(twentyOne, "twenty-one");
+	map.put(new Num(37), "thirty-seven");
+
+	const newFive = new Num(5);
+	const newTwentyOne = new Num(21);
+
+	expect(map.get(newFive)).toBe("five");
+	expect(map.get(newTwentyOne)).toBe("twenty-one");
+});
+
+test("Bulk Size/Removal Test", () => {
+
+	// Map should rehash and add new buckets as needed
+	const map = new HashMap<Num, number>();
+	expect(map.size()).toBe(0);
+
+	for (let i = 0; i < 500; i++) {
+		expect(map.remove(new Num(i))).toBe(false);
+
+		map.put(new Num(i), i);
+		expect(map.size()).toBe(i + 1);
+	}
+
+	const keys = map.keys();
+	for (const key of keys) {
+		expect(map.get(key)).toBe(key.value);
+	}
+
+	for (let i = 499; i >= 0; i--) {
+		const hasEntry = map.has(new Num(i));
+		expect(hasEntry).toBe(true);
+
+		const removed = map.remove(new Num(i));
+		expect(removed).toBe(true);
+		expect(map.size()).toBe(i);
+	}
+});
+
+test("Value Replacement Test", () => {
+	const map = new HashMap<Num, number>(64, 1.0);
+
+	for (let i = 0; i < 512; i++) {
+		map.put(new Num(i), i);
+	}
+
+	for (let i = 0; i < 512; i++) {
+		map.put(new Num(511 - i), i);
+	}
+
+	for (let i = 0; i < 512; i++) {
+		expect(map.get(new Num(i))).toBe(511 - i);
+	}
+});
---
+++ src/__tests__/LinkedListTest.test.ts
@@ -0,0 +1,59 @@
+import Num from "../test-objects/TestObjects";
+import { LinkedList } from "../";
+
+test("Bulk Insertion/Removal Test", () => {
+	const list = new LinkedList<Num>();
+	expect(list.length()).toBe(0);
+	expect(list.pop()).toBeNull();
+
+	for (let i = 0; i < 500; i++) {
+		expect(list.has(new Num(i))).toBe(false);
+		expect(list.remove(new Num(i))).toBe(false);
+
+		list.push(new Num(i));
+		expect(list.length()).toBe(i + 1);
+	}
+
+	for (let i = 499; i >= 0; i--) {
+		expect(list.pop()).not.toBeNull();
+		expect(list.length()).toBe(i);
+
+		list.peek();
+		expect(list.length()).toBe(i);
+	}
+});
+
+test("Removal/Presence Test", () => {
+	const list = new LinkedList<Num>();
+
+	for (let i = 0; i < 50; i++) {
+		expect(list.remove(new Num(i))).toBe(false);
+		list.push(new Num(i));
+	}
+
+	for (let i = 49; i >= 0; i--) {
+		const latest = list.peek();
+		expect(latest).not.toBeNull();
+
+		if (latest) {
+			expect(latest.value).toBe(i);
+		}
+
+		expect(list.has(new Num(i))).toBe(true);
+		expect(list.remove(new Num(i))).toBe(true);
+	}
+});
+
+test("Iterator Test", () => {
+	const list = new LinkedList<Num>();
+
+	for (let i = 0; i < 50; i++) {
+		list.push(new Num(i));
+	}
+
+	let index = 49;
+	for (const item of list) {
+		expect(item.equals(new Num(index))).toBe(true);
+		index--;
+	}
+});
